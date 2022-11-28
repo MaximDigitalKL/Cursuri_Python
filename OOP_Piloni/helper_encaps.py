380b162cb0f3771d3ed1b33600da362478c942dd
MaximDigitalKL <maximrazvan1990@gmail.com>
2022-11-28 17:49:34 +0200

    second commit cursuri

---
+++ OOP_Piloni/helper_encaps.py
@@ -0,0 +1,4 @@
+from OOP_Piloni.Encapsulation.Encapsulation import Casa_encapsulare_setter_getter_deleter
+
+apartament = Casa_encapsulare_setter_getter_deleter(0,2,1,"beton",40,"Strada Lalelelor 23")
+print(apartament._numar_etaje)
